74e845082130b702a2a1a585dafaa7820b2b0060
your-name <you@example.com>
2025-09-16 17:16:06 +0200

    Fxifin workflow issues

--- infra/main.tf
+++ infra/main.tf
@@ -82,3 +82,17 @@ resource "azurerm_role_assignment" "acr_pull" {
   role_definition_name = "AcrPull"
   principal_id         = azurerm_app_service.webapp.identity[0].principal_id
 }
+
+resource "azurerm_storage_account" "tf_backend" {
+  name                     = "sa${local.prefix}tf"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tf_backend.id
+  container_access_type = "private"
+}
